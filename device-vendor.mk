d5ebad63ff22b557e5001de0be429ad69deab1f3
Eskilop <eskilop@gmail.com>
2016-10-09 21:16:52 +0200

    Updated vendor blobs to NBD90Z

---
+++ device-vendor.mk
@@ -0,0 +1,66 @@
+# [2016-10-09] Auto-generated file, do not edit
+
+$(call inherit-product, vendor/lge/bullhead/bullhead-vendor-blobs.mk)
+
+# Prebuilt APKs/JARs from 'vendor/app'
+PRODUCT_PACKAGES += \
+    datastatusnotification \
+    ims
+
+# Prebuilt APKs libs symlinks from 'vendor/app'
+PRODUCT_PACKAGES += \
+    libimscamera_jni_64.so \
+    libimsmedia_jni_64.so
+
+# Prebuilt APKs/JARs from 'proprietary/app'
+PRODUCT_PACKAGES += \
+    HiddenMenu \
+    RCSBootstraputil \
+    RcsImsBootstraputil \
+    TimeService \
+    Tycho
+
+# Prebuilt APKs/JARs from 'proprietary/framework'
+PRODUCT_PACKAGES += \
+    cneapiclient \
+    com.google.widevine.software.drm \
+    qcrilhook \
+    rcsimssettings \
+    rcsservice
+
+# Prebuilt APKs/JARs from 'proprietary/priv-app'
+PRODUCT_PACKAGES += \
+    atfwd \
+    CarrierEntitlement \
+    CNEService \
+    ConnMO \
+    DCMO \
+    DiagMon \
+    DMConfigUpdate \
+    DMService \
+    Entitlement \
+    GCS \
+    HotwordEnrollment \
+    LifeTimerService \
+    qcrilmsgtunnel \
+    SprintDM
+
+# Prebuilt APKs libs symlinks from 'proprietary/priv-app'
+PRODUCT_PACKAGES += \
+    libdmjavaplugin_32.so \
+    libdmengine_32.so
+
+# Prebuilt shared libraries
+PRODUCT_PACKAGES += \
+    libloc_api_v02 \
+    libloc_ds_api \
+    libdsi_netctrl \
+    libqmi_cci \
+    libqmi_common_so \
+    libqmi_csi \
+    libqmiservices
+
+# Extra modules from user configuration
+PRODUCT_PACKAGES += \
+    ETC_Bullhead_AOSPLinks
+
